0317b23ec388541792c9aced0aac0b6e59fb095d
AmirArsalan <ghasemnia.official.com>
2024-05-23 12:56:23 +0330

    for test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import httpx
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackContext, filters, CallbackQueryHandler
 
@@ -6,9 +7,13 @@ from support_btn import support
 from order_btn import order, order_button, photo, admin_accept_btn
 from free_btn import free_acc
 
+proxies = {"http://": "socks5://127.0.0.1:2080", "https://": "socks5://127.0.0.1:2080"}
+
+
 
 def main() -> None:
-    application = Application.builder().token('6651453486:AAFC4Ff5Vr56XeuSSviG-WfazSOhNUYSiMc').build()
+    application = Application.builder().token('6651453486:AAFC4Ff5Vr56XeuSSviG-WfazSOhNUYSiMc').proxy(
+        proxy=proxies).build()
     application.add_handler(CommandHandler('start', start))
     application.add_handler(MessageHandler(filters.Regex('پشتیبانی 💁‍♀️'), support))
     application.add_handler(MessageHandler(filters.Regex('خرید سرویس  💰'), order))
